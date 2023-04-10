6275c95002e50aaebb3833d4f42a31c914376e7a
rafimps18 <impasrafael@gmail.com>
2023-04-10 15:58:45 +0800

    Improved page responsiveness and layout.

--- form.php
+++ form.php
@@ -13,36 +13,38 @@ require_once 'navbar.php';
 </head>
 
 <body>
-    <div class="form-container">
-        <div class="form-header">
-            <h1 class="formh1">Product Information Form</h1>
-        </div>
-        <form class="product-information-form">
-            <div class="information-form-content">
-                <div class="pform-item">
-                    <label class="pformLabel">Product Name</label>
-                    <input class="pformInput" type="text" placeholder="Enter product name" name="pname">
-                </div>
-                <div class="pform-item">
-                    <label class="pformLabel">Product Brand</label>
-                    <input class="pformInput" type="text" placeholder="Enter product brand" name="pbrand">
-                </div>
-                <div class="pform-item">
-                    <label class="pformLabel">Color</label>
-                    <input class="pformInput" type="text" placeholder="Enter color" name="pcolor">
-                </div>
-                <div class="pform-item">
-                    <label class="pformLabel">Price</label>
-                    <input class="pformInput" type="number" placeholder="Enter product price" name="price">
-
-                </div>
-                <div class="pform-item">
-                    <label class="pformLabel">Product Quantity</label>
-                    <input class="pformInput" type="number" placeholder="Enter product quantity" name="pquantity">
-                </div>
-                <button class="pformSubmit">Submit</button>
+    <div class="content-container">
+        <div class="form-container">
+            <div class="form-header">
+                <h1 class="formh1">Product Information Form</h1>
             </div>
-        </form>
+            <form class="product-information-form">
+                <div class="information-form-content">
+                    <div class="pform-item">
+                        <label class="pformLabel">Product Name</label>
+                        <input class="pformInput" type="text" placeholder="Enter product name" name="pname">
+                    </div>
+                    <div class="pform-item">
+                        <label class="pformLabel">Product Brand</label>
+                        <input class="pformInput" type="text" placeholder="Enter product brand" name="pbrand">
+                    </div>
+                    <div class="pform-item">
+                        <label class="pformLabel">Color</label>
+                        <input class="pformInput" type="text" placeholder="Enter color" name="pcolor">
+                    </div>
+                    <div class="pform-item">
+                        <label class="pformLabel">Price</label>
+                        <input class="pformInput" type="number" placeholder="Enter product price" name="price">
+
+                    </div>
+                    <div class="pform-item">
+                        <label class="pformLabel">Product Quantity</label>
+                        <input class="pformInput" type="number" placeholder="Enter product quantity" name="pquantity">
+                    </div>
+                    <button class="pformSubmit">Submit</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 
