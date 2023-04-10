45d18fa1a122738a11bf7aee15328b98ac36721b
rafimps18 <impasrafael@gmail.com>
2023-04-10 15:35:52 +0800

    Added changes to the site's styling.

--- navbar.php
+++ navbar.php
@@ -10,13 +10,18 @@
     <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
 </head>
 
-<body style="background-image: url('./images/background.jpg');">
-    <div class="sidenav">
-        <div class="sidenav-content">
+<body>
+    <nav>
+        <div class="sidenav-container">
             <img id="navbar-logo" src="images/logo.png">
             <h1 class="company-name-navbar">ShoeWare</h1>
-            <a href="index.php">Enter a product</a>
-            <a href="database.php">Product list</a>
-            <a href="account-settings.php">Account settings</a>
+            <div class="navbar-links">
+                <a href="index.php">Enter a product</a>
+                <a href="database.php">Product list</a>
+                <a href="account-settings.php">Account settings</a>
+            </div>
         </div>
-    </div>
+    </nav>
+</body>
+</html>
+
