bc82cfc5983e9b1853bc0f2f407fbde791cd1734
rafimps18 <impasrafael@gmail.com>
2023-04-15 20:30:31 +0800

    Created a sign up page.

--- sign-up.php
+++ sign-up.php
@@ -1,16 +1,59 @@
+<?php
+require 'dbconfig/config.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PIMS Sign Up</title>
-    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ShoeWare PIMS - Sign Up</title>
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
 </head>
+
 <body>
-    <div class="sign-up-content-container">
-        <div class="sign-up-form-container"></div>
+    <div class="container">
+        <div class="login-left">
+            <div class="login-header">
+                <h1>Product Inventory Management System</h1>
+                <p>Sign Up</p>
+            </div>
+
+            <form class="login-form" action="" method="post">
+                <div class="login-form-content">
+                    <div class="form-item">
+                        <label for="username">Name</label>
+                        <input type="text" id="username" name="uid">
+                    </div>
+                    <div class="form-item">
+                        <label for="password">Email</label>
+                        <input type="password" id="password" name="pwd">
+                    </div>
+                    <div class="form-item">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="uid">
+                    </div>
+                    <div class="form-item">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="pwd">
+                    </div>
+                    <div class="form-item">
+                        <label for="password">Re-enter Password</label>
+                        <input type="password" id="password" name="pwd">
+                    </div>
+                    <button type="submit" name="submit">Sign Up</button>
+                </div>
+            </form>
+        </div>
+        <div class="login-right">
+            <div class="company-logo-and-name">
+                <img class="company-logo" src=".\images\logo.png" alt="">
+                <h1 class="company-name">ShoeWare</h1>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
