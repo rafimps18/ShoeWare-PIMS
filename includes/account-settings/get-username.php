df1cd9aac55a566d6f363793784273deaa737ac5
rafimps18 <impasrafael@gmail.com>
2023-04-17 20:55:05 +0800

    made some changes

---
+++ includes/account-settings/get-username.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$id = $_SESSION["userid"]; // Replace with the ID value you have
+
+// MySQL query to retrieve username based on ID value
+$sql = "SELECT usersUid FROM users WHERE usersId = $id";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of the first row (assuming ID is unique)
+    $row = $result->fetch_assoc();
+    $username = $row["usersUid"];
+    echo "Username: " . $username;
+} else {
+    echo "No user found with the given ID";
+}
+
+$conn->close();
