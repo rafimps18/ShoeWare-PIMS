9aee5d6f54952073f0e216bb0414e403bb9bdfc8
rafimps18 <impasrafael@gmail.com>
2023-04-19 20:53:16 +0800

    Deleted some unnecessary files.

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function showPopup() {
-  document.getElementById("popup").style.display = "block";
-  document.getElementById("overlay").style.display = "block";
-}
-
-function hidePopup() {
-  document.getElementById("popup").style.display = "none";
-  document.getElementById("overlay").style.display = "none";
-}
