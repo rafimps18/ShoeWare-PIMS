0d16256d276c81dffcf31676d20e0dfaeaecae82
rafimps18 <impasrafael@gmail.com>
2023-05-17 22:31:57 +0800

    Fixed all the issues raised in the QA including adding a landing page.

--- landing-page.php
+++ landing-page.php
@@ -1,77 +1,24 @@
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Landing Page</title>
     <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            margin: 0;
-            padding: 0;
-            font-family: sans-serif;
-        }
-
-        .logo-container {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-bottom: 40px;
-        }
-
-        .logo {
-            margin-right: 20px;
-        }
-
-        .company-name {
-            font-size: 24px;
-            font-weight: bold;
-        }
-
-        .button-container {
-            display: flex;
-            align-items: center;
-            margin-top: 40px;
-        }
-
-        .button {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            margin: 0 20px;
-            background-color: #007bff;
-            color: #fff;
-            font-size: 24px;
-            font-weight: bold;
-            text-align: center;
-            text-decoration: none;
-            transition: all 0.3s ease;
-        }
-
-        .button:hover {
-            background-color: #0056b3;
-            transform: scale(1.03);
-        }
-    </style>
+    <link rel="stylesheet" href="css\landing-page-styling.css">
 </head>
 
 <body>
     <div class="content-container">
         <div class="logo-container">
             <div class="logo">
-                <img src="images/logo.png" alt="Logo" width="100">
+                <img src="images/logo.png" class="logo" alt="Logo" width="100">
             </div>
             <div class="company-name">
                 Shoeware
             </div>
         </div>
+        <h3 class="website-name">Product Inventory Management System</h3>
         <div class="button-container">
             <a href="form.php" class="button">Inventory Form</a>
             <a href="database.php" class="button">Product Catalog</a>
